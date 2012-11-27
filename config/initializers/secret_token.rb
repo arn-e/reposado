27eafd56ae5ea513eb093847771230d18d3263c7
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-27 09:24:32 -0800

    Iniital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Reposado::Application.config.secret_token = '16bc1f991a60b941a9c6617e9219038c57a485489ec2dea42d41e510db250ce6ebc9536337617af5c22037c2d672061637c1b49032baeaf8a01fa970c933e0bd'
