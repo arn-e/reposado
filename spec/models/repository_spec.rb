7f79b0018fa7d31601f81ec31847c723d87ee61e
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-28 19:37:17 -0800

    Add methods for processing and formatting data to JSON

--- spec/models/repository_spec.rb
+++ spec/models/repository_spec.rb
@@ -25,45 +25,42 @@ describe Repository do
 
     let (:repo) { Repository.first }
 
-    describe "#top_users_by_commits" do
+    describe "#users_by_commits" do
       it "has number of user commits" do
-        @data = repo.top_users_by_commits
-        @data.keys.first.should eq "octocat"
-        @data.values.first.should eq 6
+        @data = repo.users_by_commits
+        @data[:user_commit_counts].keys.first.should eq "octocat"
+        @data[:user_commit_counts].values.first.should eq 6
 
-        @data.keys.last.should eq "user2"
-        @data.values.last.should eq 4
+        @data[:user_commit_counts].keys.last.should eq "user2"
+        @data[:user_commit_counts].values.last.should eq 4
       end
     end
 
-    describe "#top_users_by_comments" do
+    describe "#users_by_comments" do
       it "has number of user comments" do
-        @data = repo.top_users_by_comments
-        @data.keys.first.should eq "octocat"
-        @data.values.first.should eq 6
+        @data = repo.users_by_comments
+        @data[:user_comment_counts].keys.first.should eq "octocat"
+        @data[:user_comment_counts].values.first.should eq 6
 
-        @data.keys.last.should eq "user2"
-        @data.values.last.should eq 7
+        @data[:user_comment_counts].keys.last.should eq "user2"
+        @data[:user_comment_counts].values.last.should eq 7
       end
     end
 
     describe "#activity_by_week" do
       it "has dates" do
-        @data = repo.activity_by_week
-        @data.first[:date].should eq DateTime.strptime("2011-04-14T16:00:49Z")
-        @data.first[:activities].should eq 30
-
-        # [ { :date => DATETIME_OBJ, :activities => INT },
-        #   { :date => DATETIME_OBJ, :activities => INT }
-        # ]
+        pending
+        # @data = repo.activity_by_week
+        # @data.first[:date].should eq DateTime.strptime("2011-04-14T16:00:49Z")
+        # @data.first[:activities].should eq 30
       end
     end
 
     describe "#to_json" do
-      it "converts a Ruby object to JSON" do
-        @data = repo.top_users_by_commits
-        @json = @data.to_json
-        @json.should be_instance_of JSON
+      it "correctly formats a hash" do
+        @data = repo.users_by_commits
+        @json = repo.to_json(@data)
+        @json.should eq "{\"user_commit_counts\":{\"octocat\":6,\"user2\":4}}"
       end
     end
   end
