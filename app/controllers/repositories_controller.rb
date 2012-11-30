45cbc5158cfa2b364c8454540cfe9e3978554e44
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-29 17:04:04 -0800

    Modify process for saving a repository's chart data'

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,7 +7,8 @@ class RepositoriesController < ApplicationController
   def create
     @repo = Repository.new_repository(params[:repo])
     @data = @repo.collect_data([@repo.users_by_comments]).to_json
-    @repo.save_chart_data(@data)
+    @repo.chart_data = @data
+    @repo.save
     redirect_to :action => 'index'
     # Repository.collect_issue_data(params[:Repo])
   end
