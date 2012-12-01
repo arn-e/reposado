a481bc70e146ff5464ee0b408496b4c6d9664c8a
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-30 19:43:16 -0800

    preparing to merge local master

--- lib/github_data_processor.rb
+++ lib/github_data_processor.rb
@@ -1,12 +1,18 @@
 module GithubDataProcessor
   def users_by_commits
     counts = Hash.new(0)
-    result = []
     commits.group(:user).count.each do |commit_name, count|
       counts[commit_name] += count
-      result.push({"name" => commit_name, "num" => counts[commit_name]})
     end
-    { :commiters => result }
+
+    result = []
+    counts.each do |key,value|
+      formatted_count = Hash.new
+      formatted_count["name"] = key
+      formatted_count["num"] = value
+      result.push(formatted_count)
+    end
+    { :committers => result }
   end
 
   #{"octocat"=>5}
@@ -16,11 +22,10 @@ module GithubDataProcessor
     issues.each do |issue|
       issue.comments.group(:user).count.each do |comment_name, count|
         counts[comment_name] += count
-        # result.push({"name" => comment_name, "num" => counts[comment_name]})
       end
     end
-    result = []
 
+    result = []
     counts.each do |key,value|
       formatted_count = Hash.new
       formatted_count["name"] = key
@@ -28,11 +33,6 @@ module GithubDataProcessor
       result.push(formatted_count)
     end
 
-    # combined_results = Hash.new(0)
-    # result.each {|hash| hash.each {|key, value| combined_results[key] += value.to_i}}
-
-    # {"username"=>20, "username2"=>15}
-    # { {"name"=>"username", "num"=>20}, {"name"=>username2", "num"=>15} }
     { :committers => result }  #  ###################### do not forget to change this back
   end
 
