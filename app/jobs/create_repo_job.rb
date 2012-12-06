cd6fc8c29a8d1e628b61e0c505ccedf1e5cfca4f
dbc-apprentice <apprentice@devbootcamp.com>
2012-12-05 22:29:57 -0800

    updated create_repo_job with new data inputs

--- app/jobs/create_repo_job.rb
+++ app/jobs/create_repo_job.rb
@@ -5,7 +5,7 @@ class CreateRepoJob
 
   def perform
     @repo = Repository.from_url(@repo_url)
-    @data = @repo.collect_data([@repo.users_by_commits, @repo.users_by_comments, @repo.relevant_words]).to_json
+    @data = @repo.collect_data([@repo.users_by_commits, @repo.users_by_comments, @repo.relevant_words, @repo.response_time_for_issues, @repo.average_response_time]).to_json
     @repo.chart_data = @data
     @repo.save!
   end
