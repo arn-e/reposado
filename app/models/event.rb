28a81e7856f29a88d266c07ba5ffd5f2fa408052
dbc-apprentice <apprentice@devbootcamp.com>
2012-12-05 20:50:38 -0800

    resolved json login value bug; added thread code in repository model which needs to be tested in production

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,7 +6,11 @@ class Event < ActiveRecord::Base
   def self.from_json(json)
     event         = self.new
     event.date    = DateTime.parse(json["created_at"])
-    event.git_user    = json["actor"]["login"]
+    if json["actor"] != nil
+      event.git_user    = json["actor"]["login"]
+    else
+      event.git_user    = ""
+    end
     event.status  = json["event"]
     # event.body    = json["body"]
     event
