4adf1c96da3fa559d576e1267e8085eb91874ad6
dbc-apprentice <apprentice@devbootcamp.com>
2012-12-05 20:04:34 -0800

    added de-duplication to commit model

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,4 +1,41 @@
 class Commit < ActiveRecord::Base
   belongs_to :repository
   validates_presence_of :git_user, :date
+
+  def self.most_recent_commit(repo_id)
+    most_recent_commit = DateTime.parse("1900-12-02 22:39:44")
+    Repository.find(repo_id).commits.each do |commit|
+      (most_recent_commit = commit.date) if commit.date > most_recent_commit
+    end
+    most_recent_commit
+  end
+
+  def self.update_commit_data(commit_data, repo_id, sha_collection)
+    commit_data.each do |commit|
+      logger.debug("error : #{commit}")
+      @new_commit = Commit.new
+      @new_commit.repository_id = repo_id
+      @new_commit.sha = commit["sha"]
+      if commit["parents"].length != 0
+        @new_commit.parent_sha = commit["parents"][0]["sha"] # add multiple parents?
+      end
+      if commit["commit"] != nil
+        @new_commit.message = commit["commit"]["message"]
+      end
+      if commit["committer"] != nil
+        if commit["committer"]["login"] == nil
+          @new_commit.git_user = commit["commit"]["committer"]["name"]
+        elsif commit["committer"]["login"] != nil
+          @new_commit.git_user = commit["committer"]["login"]
+        else
+          @new_commit.git_user = " "
+        end
+        @new_commit.date = DateTime.parse(commit["commit"]["committer"]["date"])
+
+        @new_commit.save if sha_collection[@new_commit.sha].nil?
+
+      end
+    end
+  end
+
 end
--- app/models/repository.rb
+++ app/models/repository.rb
@@ -32,42 +32,44 @@ class Repository < ActiveRecord::Base
     open_data         = GithubHandler.query_github(repo_path, "open")
     closed_data       = GithubHandler.query_github(repo_path, "closed")
 
-    threads = []
+    # threads = []
+
+    # open_data.each do |issue|
+    #   threads << Thread.new { Issue.from_json(issue, repo_id) }
+    # end
+
+    # closed_data.each do |issue|
+    #   threads << Thread.new { Issue.from_json(issue, repo_id) }
+    # end
+
+    # threads.join #waits for stuff to finish, bubbles exceptions
 
     open_data.each do |issue|
-      threads << Thread.new { Issue.from_json(issue, repo_id) }
+      Issue.from_json(issue, repo_id)
     end
 
     closed_data.each do |issue|
-      threads << Thread.new { Issue.from_json(issue, repo_id) }
+      Issue.from_json(issue, repo_id)
     end
 
-    threads.join #waits for stuff to finish, bubbles exceptions
-
-    # update_issue_data(open_data, repo_id)
-    # update_issue_data(closed_data, repo_id)
-
-    # collect_issue_data(repo_path, open_data)
-    # collect_issue_data(repo_path, closed_data)
   end
-
-  # def self.collect_issue_data(repo_path, data)
-  #   data_types = ["comments", "events"]
-  #   data.each do |issue|
-  #     data_types.each do |data_type|
-  #       comment_data = GithubHandler.query_github_issue_data(repo_path, issue["number"].to_i, data_type)
-  #       update_issue_child_data(comment_data, issue["number"].to_i, data_type) unless comment_data.length < 1
-  #     end
-  #   end
-  # end
 
   def self.collect_commits(repo_path, repo_id)
     branches = collect_branches(repo_path)
-    puts "************************** #{branches.inspect}"
     branches.each do |branch|
       branch_name, branch_start_sha = branch["name"], branch["commit"]["sha"]
-      commit_data = collect_commit_page(repo_path, repo_id, branch_name, branch_start_sha)
-      update_commit_data(commit_data, repo_id) unless commit_data.nil? || commit_data.length < 1
+      commits_gathered = false
+
+      until commits_gathered
+        # TODO : Update sha_collection based on internal created_date
+        sha_collection = {}
+        Repository.find(repo_id).commits.each {|commit| sha_collection[commit.sha] = 1}
+        commit_data = collect_commit_page(repo_path, repo_id, branch_name, branch_start_sha)
+        Commit.update_commit_data(commit_data, repo_id, sha_collection) unless commit_data.nil? || commit_data.length < 1
+        branch_start_sha = commit_data[-1]["sha"]
+        (commits_gathered = true) if commit_data.length < 100
+      end
+
     end
 
   end
@@ -79,69 +81,5 @@ class Repository < ActiveRecord::Base
   def self.collect_branches(repo_path)
     GithubHandler.query_github_branches(repo_path)
   end
-
-  def self.update_commit_data(commit_data, repo_id)
-    commit_data.each do |commit|
-      logger.debug("error : #{commit}")
-      @new_commit = Commit.new
-      @new_commit.repository_id = repo_id
-      @new_commit.sha = commit["sha"]
-      if commit["parents"].length != 0
-        @new_commit.parent_sha = commit["parents"][0]["sha"] # add multiple parents?
-      end
-      if commit["commit"] != nil
-        @new_commit.message = commit["commit"]["message"]
-      end
-      if commit["committer"] != nil
-        if commit["committer"]["login"] == nil
-          @new_commit.git_user = commit["commit"]["committer"]["name"]
-        elsif commit["committer"]["login"] != nil
-          @new_commit.git_user = commit["committer"]["login"]
-        else
-          @new_commit.git_user = " "
-        end
-        @new_commit.date = DateTime.parse(commit["commit"]["committer"]["date"])
-        @new_commit.save!
-      end
-    end
-  end
-
-  # def self.update_issue_child_data(data, issue_number, data_type)
-  #   data.each do |issue_data|
-
-  #     case data_type
-  #     when "comments"
-  #       @new_issue_data = Comment.new
-  #       puts "***************************"
-  #       puts "MOST RECENT ISSUE ID: #{Issue.last.id}"
-  #       puts "***************************"
-  #       @new_issue_data.issue_id = Issue.last.id
-  #     when "events"
-  #       @new_issue_data = Event.new
-  #       @new_issue_data.issue_id = Issue.last.id
-  #     end
-
-  #     puts "*****************************"
-  #     puts "NEW_ISSUE_DATA: #{@new_issue_data}"
-  #     puts "*****************************"
-
-  #     if data_type == "comments"
-  #       @new_issue_data.body = issue_data["body"]
-  #       @new_issue_dait.git_user = issue_data["user"]["login"]
-  #     elsif data_type == "events"
-  #       @new_issue_dait.git_user = issue_data["actor"]["login"]
-  #       @new_issue_data.status = issue_data["event"]
-  #     end
-  #     @new_issue_data.date = Date.strptime(issue_data["created_at"])
-  #     @new_issue_data.save!
-  #   end
-  # end
-
-
-      # @new_issue.repository_id = repo_id
-      # new_issue.state = issue["state"]
-      # @new_issue.save!
-
-
 
 end
